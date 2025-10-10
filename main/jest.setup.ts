ea5e39de6a7eb9995523e0b92bf00b9e09477845
gaureshpai <paigauresh@gmail.com>
2025-10-10 06:38:08 +0530

    test build issue res

--- main/jest.setup.ts
+++ main/jest.setup.ts
@@ -1,6 +1,8 @@
 import '@testing-library/jest-dom';
 
-// Polyfill for TextEncoder and TextDecoder
-import { TextEncoder, TextDecoder } from 'util';
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder as any; // Cast to any to avoid TS errors if TextDecoder types are slightly off
+// Polyfill for TextEncoder and TextDecoder, only in test environment
+if (typeof global.TextEncoder === 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util');
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+}
